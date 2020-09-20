0c744cca50cf2af9e2e09a5545f86a2458e5965b
lacrimosa <v76paastaminutvapaaksi@gmail.com>
2020-09-20 22:59:30 +0900

    Initial Commit

---
+++ la programmazione 2°/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  la programmazione 2°
+//
+//  Created by CSQuattro on 2020/09/20.
+//  Copyright © 2020 CSQuattro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
